9a626bd6c0765eb9a21f36009a8df5380f18a78c
Unknown <admars.96@gmail.com>
2017-11-27 15:53:56 +1100

    addMessage
    Created the addMessage function which adds messages to the recentposts div by creating a new paragraph element to store the inputted text. Works on firefox, chrome, and safari

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const postFunction = () => {
-	console.log(document.getElementById('postedMessage').value);
+//Posts to console by retrieving postedMessage from the submit form
+//const postFunction = () => {
+	//console.log(document.getElementById('postedMessage').value); //prints the text from the input form to console
+//};
+
+//Adds message to the page in the same form
+function addMessage() {
+	 // create a new p element 
+  var newP = document.createElement("p");
+
+  // and give it some content 
+  var newPost = document.createTextNode(document.getElementById('postedMessage').value); 
+  
+  // add the text node to the newly created div
+  newP.appendChild(newPost);  
+  console.log(newP);
+  console.log(newP.appendChild(newPost));
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("recentposts"); 
+  currentDiv.appendChild(newP); 
 };
+
+//button over form in javascript
+
+
 
 	
